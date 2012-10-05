dd97530bee53640aeec554d2925abbd279e75ea6
Hao Wu <haowu@cs.utexas.edu>
2012-10-05 14:48:18 -0500

    new version of design

---
+++ code/src/single/vcs/vcs.h
@@ -0,0 +1,12 @@
+// The file describes the interface for a Version Control System (VCS)
+
+#ifndef SINGLE_VCS_VCS_H_
+#define SINGLE_VCS_VCS_H_
+
+// The interface
+class vcs {
+    public:
+    // TODO implement
+}
+
+#endif // SINGLE_VCS_VCS_H_
