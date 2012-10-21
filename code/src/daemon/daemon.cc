dc95a1b27ad72c52680d4aa23d2bfa47166e14e0
Hao Wu <haowu@cs.utexas.edu>
2012-10-20 19:25:24 -0500

    daemon

---
+++ code/src/daemon/daemon.cc
@@ -0,0 +1,9 @@
+// The FVM Daemon
+
+#include <daemon/daemon.h>
+
+int main()
+{
+    return 0;
+}
+
---
+++ code/src/daemon/daemon.h
@@ -0,0 +1,18 @@
+// The file describes the FVM Daemon
+
+#ifndef DAEMON_DAEMON_H_
+#define DAEMON_DAEMON_H_
+
+#include <map>
+#include <string>
+
+#include <common/common.h>
+
+class Daemon {
+    public:
+
+    private:
+};
+
+#endif // DAEMON_DAEMON_H_
+
